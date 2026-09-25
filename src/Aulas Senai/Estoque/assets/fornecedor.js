f499522308d7561bb5ea3c3ad422eea507bdf9ce
Davi Profeta <davihpg2008@gmail.com>
2026-09-25 09:40:15 -0300

    Refactor fornecedor.js to handle multiple buttons

--- src/Aulas Senai/Estoque/assets/fornecedor.js
+++ src/Aulas Senai/Estoque/assets/fornecedor.js
@@ -1,16 +1,44 @@
-const abasBTN = document.querySelector(".abas");
+const abasBTN = document.querySelectorAll(".abas");
 const abas = document.querySelector(".header");
+const consultar = document.getElementById("consultar");
+const cadfornecedor = document.getElementById("cadFornecedor");
+const produto = document.getElementById("cadProduto");
 
-abasBTN.addEventListener("click", e => {
+abasBTN[0].addEventListener("click", e => {
     window.location.href = "../index.php";
+    btnConsultar.style.display = "flex"
+    divCadastrar.style.display = "none";
+    main.style.display = "none";
+    fornecedor.style.display = "none";
+});
+
+abasBTN[1].addEventListener("click", e => {
+    window.location.href = "../index.php";
+    fornecedor.style.display = "block";
+    mainFor.style.display = "flex";
+    main.style.display = "none";
+    divCadastrar.style.display = "none";
+    btnConsultar.style.display = "none"
+});
+
+abasBTN[2].addEventListener("click", e => {
+    window.location.href = "../index.php";
+    divCadastrar.style.display = "block";
+    main.style.display = "none";
+    fornecedor.style.display = "none";
+    btnConsultar.style.display = "none"
 });
 
 abas.addEventListener("mouseenter", e => {
     abas.style.width = "20vw";
-    consultar.textContent = "Voltar";
+    consultar.textContent = "Consultar";
+    cadfornecedor.textContent = "Fornecedor";
+    produto.textContent = "Produto";
 });
 
 abas.addEventListener("mouseleave", e => {
     abas.style.width = "8vw";
     consultar.textContent = "";
+    cadfornecedor.textContent = "";
+    produto.textContent = "";
 });
